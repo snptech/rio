d2749be3acee9b377219e0ea152f560ea76e59f8
Amar <amar@cdat.co.in>
2021-05-07 15:12:00 +0530

    update

--- resources/views/header/navigation.blade.php
+++ resources/views/header/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
       <li class="nav-item active">
-        <a class="nav-link" href="{{ route("/home") }}">
+        <a class="nav-link" href="{{ route("home") }}">
           <i class="menu-icon" data-feather="home"></i>
           <span class="menu-title">Dashboard</span>
         </a>
