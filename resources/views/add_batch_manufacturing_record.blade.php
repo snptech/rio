4f5032f664e7afedfacfea1e70b67ebac67f2059
Amar <amar@cdat.co.in>
2021-06-28 18:03:52 +0530

    update

--- resources/views/add_batch_manufacturing_record.blade.php
+++ resources/views/add_batch_manufacturing_record.blade.php
@@ -173,103 +173,113 @@
                         </form>
 					</div>
 					<div id="billOfRawMaterial" class="tab-pane fade">
-						<div class="form-row">
-							<div class="col-12 col-md-6 col-lg-6 col-xl-6">
-								<div class="form-group">
-								  <label for="proName" class="active">Product Name</label>
-								  <input type="text" class="form-control" id="proName" placeholder="Product Name" value="Simethicone (Filix-110)">
-								</div>
-							</div>
-							<div class="col-12 col-md-6 col-lg-6 col-xl-6">
-								<div class="form-group">
-								  <label for="bmrNo" class="active">BMR No.</label>
-								  <input type="text" class="form-control" id="bmrNo" placeholder="BMR No." value="RCIPL/BMR/Flx-2300/09">
-								</div>
-							</div>
-							<div class="col-12 col-md-6 col-lg-6 col-xl-6">
-								<div class="form-group">
-								  <label for="batchNo">Batch No.</label>
-								  <input type="text" class="form-control" id="batchNo" placeholder="Batch No." value="RFLX 20/668">
-								</div>
-							</div>
-							<div class="col-12 col-md-6 col-lg-6 col-xl-6">
-								<div class="form-group">
-								  <label for="refMfrNo">Ref. MFR No.</label>
-								  <input type="text" class="form-control" id="refMfrNo" placeholder="Ref. MFR No." value="RCIPL/MFR/01/01">
-								</div>
-							</div>
-							<div class="col-12 col-md-12 col-lg-12 col-xl-12">
-								<div class="form-group input_fields_wrap2" id="MaterialReceived">
-									<label class="control-label d-flex">Bill of Raw Material Details and Weighing Record
-										<div class="input-group-btn">
-											<button class="btn btn-dark add-more add_field_button2 waves-effect waves-light" type="button">Add More +</button>
-										</div>
-									</label>
-									<div class="row add-more-wrap after-add-more m-0 mb-4">
-										<!-- <span class="add-count">1</span> -->
-										<div class="col-12 col-md-6 col-lg-4">
-											<div class="form-group">
-											  <label for="rawMaterialName" class="active">Raw Material</label>
-											  <select class="form-control select" id="rawMaterialName">
-												<option>Select</option>
-												<option>Material Name</option>
-											  </select>
-											</div>
-										</div>
-										<div class="col-12 col-md-6 col-lg-4">
-											<div class="form-group">
-											  <label for="batchNo" class="active">Batch No.</label>
-											  <select class="form-control select" id="batchNo">
-												<option>Select</option>
-												<option>RFLX</option>
-											  </select>
-											</div>
-										</div>
-										<div class="col-12 col-md-6 col-lg-4">
-											<div class="form-group">
-											  <label for="Quantity" class="active">Quantity (Kg.)</label>
-											  <input type="text" class="form-control" id="Quantity" placeholder="">
-											</div>
-										</div>
-										<div class="col-12 col-md-6 col-lg-4">
-											<div class="form-group">
-											  <label for="arNo" class="active">AR No.</label>
-											  <input type="text" class="form-control" id="arNo" placeholder="">
-											</div>
-										</div>
-										<div class="col-12 col-md-6 col-lg-4">
-											<div class="form-group">
-											  <label for="date" class="active">Date</label>
-											  <input type="date" class="form-control calendar" id="date" placeholder="">
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="col-12 col-md-6">
-								<div class="form-group">
-								  <label for="doneBy">Weighed by</label>
-								  <select class="form-control select" id="doneBy">
-									<option>Select</option>
-									<option>Employee Name</option>
-								  </select>
-								</div>
-							</div>
-							<div class="col-12 col-md-6">
-								<div class="form-group">
-								  <label for="checkedBy">Checked by</label>
-								  <select class="form-control select" id="checkedBy">
-									<option>Select</option>
-									<option>Employee Name</option>
-								  </select>
-								</div>
-							</div>
-							<div class="col-12">
-								<div class="form-group">
-								  <button type="submit" class="btn btn-primary btn-md ml-0 form-btn waves-effect waves-light">Submit & Next</button><button type="clear" class="btn btn-light btn-md form-btn waves-effect waves-light">Save & Quite</button>
-								</div>
-							</div>
-						</div>
+						<form id="add_batch_manufacturing" method="post" action="{{ route('add_batch_manufacturing_recorde_insert') }}">
+                            @csrf
+
+                        <div class="form-row">
+                            <div class="col-12 col-md-6 col-lg-6 col-xl-6">
+                                <div class="form-group">
+                                    <label for="proName" class="active">Product Name</label>
+                                    <input type="text" class="form-control" name="proName" id="proName" placeholder="Product Name" value="Simethicone (Filix-110)">
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-6 col-xl-6">
+                                <div class="form-group">
+                                    <label for="bmrNo" class="active">BMR No.</label>
+                                    <input type="text" class="form-control" name="bmrNo" id="bmrNo" placeholder="BMR No." value="RCIPL/BMR/Flx-2300/09">
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-6 col-xl-6">
+                                <div class="form-group">
+                                    <label for="batchNoI">Batch No.</label>
+                                    <input type="text" class="form-control" name="batchNoI" id="batchNoI" placeholder="Batch No." value="RFLX 20/668">
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-6 col-xl-6">
+                                <div class="form-group">
+                                    <label for="refMfrNo">Ref. MFR No.</label>
+                                    <input type="text" class="form-control" name="refMfrNo" id="refMfrNo" placeholder="Ref. MFR No." value="RCIPL/MFR/01/01">
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-12 col-lg-12 col-xl-12">
+                                <div class="form-group input_fields_wrap" id="MaterialReceived">
+                                    <label class="control-label d-flex">Bill of Raw Material Details and Weighing Record
+                                        <div class="input-group-btn">
+                                            <button class="btn btn-dark add-more add_field_button waves-effect waves-light" type="button">Add More +</button>
+                                        </div>
+                                    </label>
+                                    <div class="row add-more-wrap after-add-more m-0 mb-4">
+                                        <!-- <span class="add-count">1</span> -->
+                                        <div class="col-12 col-md-6 col-lg-4">
+                                            <div class="form-group">
+                                                <label for="rawMaterialName" class="active">Raw Material</label>
+                                                <select class="form-control select" name="rawMaterialName[]" id="rawMaterialName">
+                                                    <option>Select</option>
+                                                    <option>Material Name</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 col-md-6 col-lg-4">
+                                            <div class="form-group">
+                                                <label for="batchNo" class="active">Batch No.</label>
+                                                <select class="form-control select" name="batchNo[]" id="batchNo">
+                                                    <option>Select</option>
+                                                    <option>RFLX</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 col-md-6 col-lg-4">
+                                            <div class="form-group">
+                                                <label for="Quantity" class="active">Quantity (Kg.)</label>
+                                                <input type="text" class="form-control" name="Quantity[]" id="Quantity" placeholder="">
+                                            </div>
+                                        </div>
+                                        <div class="col-12 col-md-6 col-lg-4">
+                                            <div class="form-group">
+                                                <label for="arNo" class="active">AR No.</label>
+                                                <input type="text" class="form-control" name="arNo[]" id="arNo" placeholder="">
+                                            </div>
+                                        </div>
+                                        <div class="col-12 col-md-6 col-lg-4">
+                                            <div class="form-group">
+                                                <label for="date" class="active">Date</label>
+                                                <input type="date" class="form-control calendar" name="date[]" id="date" value={{ date("Y-m-d") }}>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="doneBy">Weighed by</label>
+                                    <select class="form-control select" name="doneBy" id="doneBy">
+                                        <option>Select</option>
+                                        <option>Employee Name</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="checkedBy">Checked by</label>
+                                    <select class="form-control select" name="checkedBy" id="checkedBy">
+                                        <option>Select</option>
+                                        <option>Employee Name</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                      <label for="Remark" class="active">Note / Remark</label>
+                                      <textarea class="form-control" name="Remark" id="Remark" placeholder="Note / Remark"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                      <button type="submit" class="btn btn-primary btn-md ml-0 form-btn waves-effect waves-light">Submit & Next</button><button type="clear" class="btn btn-light btn-md form-btn waves-effect waves-light">Save & Quite</button>
+                                    </div>
+                                </div>
+                        </div>
+                    </form>
 					</div>
 					<div id="listOfEquipment" class="tab-pane fade">
 						<div class="form-row">
