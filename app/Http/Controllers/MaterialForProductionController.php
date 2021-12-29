da74103e0565e31d7eec96af47bad0513fecfc6e
Amar Cdat <amar@cdat.co.in>
2021-12-29 19:14:55 +0530

    update

--- app/Http/Controllers/MaterialForProductionController.php
+++ app/Http/Controllers/MaterialForProductionController.php
@@ -221,7 +221,7 @@ class MaterialForProductionController extends Controller
             //
             ->where("packing_material_requisition_slip.id",$request->id)
             ->first();
-
+dd($data['issue_material']);
             $data["material_details"] = DetailsRequisition::select("detail_packing_material_requisition.*",
             "raw_materials.material_name","detail_packing_material_requisition.id as details_id")
             ->where("requisition_id",$data["issue_material"]->id)
